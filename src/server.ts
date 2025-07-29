2d0ab9c03a3e0e6868855fe0e97c31407ae98a1e
Shivam gond <codewithharry35434@gmail.com>
2025-07-29 20:20:45 +0530

    sd

--- src/server.ts
+++ src/server.ts
@@ -6,3 +6,16 @@ const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
 });
+// / Only listen when not in production (for local development)
+if (process.env.NODE_ENV !== "production") {
+  app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+  });
+}
+
+// "scripts": {
+//     "build": "npx tsc",
+//     "start": "node dist/server.js",
+//     "dev": "nodemon --exec ts-node src/server.ts",
+//     "vercel-build": "npx tsc"
+//   }
